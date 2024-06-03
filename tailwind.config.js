85cc64a64c8e47547d67b5b15a515a942e0f4db2
Israel4u <israeloluwasholao@gmail.com>
2024-06-02 20:27:16 -0700

    Completed the creations section with its mobile version.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
         alata: ['Alata'],
+      },
+      letterSpaciing: {
+        widest: '.3em',
       }
     },
   },
